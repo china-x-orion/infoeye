801022749168affc8173f73a03b962c3ad8595fe
rocky <jingzheng.w@gmail.com>
2014-08-06 20:19:15 +0800

    just finished arp module

--- tools/arp.py
+++ tools/arp.py
@@ -3,47 +3,22 @@
 Author:rockylinux
 E-mail:Jingzheng.W@gmail.com
 """
-import os
+import commands 
 
 #display ethe's ip and MAC
+#return a list containning title and arp item
 class arp:
     def __init__(self):
 	    self.__name = 'arp'
 
     def getData(self):
-	    result = os.system('if [ -e /usr/sbin/arp ] ; then /usr/sbin/arp ; else /sbin/arp ; fi | awk \'BEGIN {OFS=","} {print $1,$2,$3,$4,$5}\'')
-        print result
+        (status, output) = commands.getstatusoutput('if [ -e /usr/sbin/arp ] ; then /usr/sbin/arp ; else /sbin/arp ; fi | awk \'BEGIN {OFS=","} {print $1,$2,$3,$4,$5}\'')
+        return output.split() 
+
+    def testGetData(self,test):
+        print test 
 
 if __name__ == '__main__':
     a = arp()
-    a.getData()
-"""
-<?php
-    namespace Modules;
-
-    class arp extends \ld\Modules\Module {
-
-        protected $name = 'arp';
-
-        public function getData($args=array()) {
-
-	    exec('if [ -e /usr/sbin/arp ] ; then /usr/sbin/arp ; else /sbin/arp ; fi | awk \'BEGIN {OFS=","} {print $1,$2,$3,$4,$5}\'', $result);
-
-	    $data = array();
-	    $x = 0;
-
-	    foreach ($result as $a) {
-                if ($x==0) {
-                    $x++;
-                    continue;
-                }
-                $data[] = explode(',', $result[$x]);
-
-                unset($result[$x], $a);
-                $x++;
-            }
-
-            return $data;
-        }
-    }
-"""
+    test = a.getData()
+    a.testGetData(test)
